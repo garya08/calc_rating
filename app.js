48a43a052495edfe777a1e3f0cf54bde38552b36
Arten <garyaev_ar10@mail.ru>
2024-08-19 01:02:43 +0300

    styling js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 // ------------- КОНСТАНТЫ ------------- 
+const red = '#ff0000',
+      main_clr = '#00822b';
+
 const maxLength = 4,
       K = 5, // коэффициент влияния клиентских отзывов
-      regEx = /\D/g;
-
+      regEx = /\D/g,
+      goodRating = 4.5;
 
 // ------------- ОСНОВНЫЕ ЭЛЕМЕНТЫ СТРАНИЦЫ -------------  
 // Кол-во оценок Клиентов и Сервиса, Средний чек, поле результата, список элементов оценок клиентов и сервиса
 const avgInputEl = document.querySelector('#avg'),
-      serviceCountEl = document.querySelector('#count_auto'),
-      clientCountEl = document.querySelector('#count_client'),
+      serviceCountEl = document.querySelector('#serviceCount'),
+      clientCountEl = document.querySelector('#clientCount'),
       resultEl = document.querySelector('#result'),   // input результата
+      btnResult = document.querySelector('#btn_result'),
       serviceReviewsEl = document.querySelectorAll('.service_review'),  // список input автоматические оценки
       clientReviewsEl = document.querySelectorAll('.client_review');  // список input оценки клиентов
 
@@ -50,18 +54,10 @@ const toggleInputs = (countInput, inputsList) =>
             }
         }
     })
-//функция для подсчета суммы всех полей input
-const summarize = function(list) {
-    let sum = 0;
-    for(let item of list) {
-        sum += Number(item.value);
-    }
-    return sum;
-}
 // функция для отслеживания ошибки превышения общего количества
 const wrongInputs = (inputsList, countInput) => inputsList.forEach((item) => {
     item.addEventListener('input', (e)=> {
-        if(summarize(inputsList) > Number(countInput.value)) {
+        if(checkSumWrong(inputsList) > Number(countInput.value)) {
             e.target.parentNode.classList.add('wrong');
             e.target.classList.add('wrong');
         } else {
@@ -74,6 +70,29 @@ const wrongInputs = (inputsList, countInput) => inputsList.forEach((item) => {
         }
     })
 })
+// вычисление рейтинга
+btnResult.addEventListener('click', () => {
+    let service = calcReviews(serviceReviewsEl) * parseInt(avgInputEl.value);
+    let client = K * calcReviews(clientReviewsEl) * parseInt(avgInputEl.value);
+
+    let up =  service + client;
+    let down = parseInt(avgInputEl.value) * (parseInt(serviceCountEl.value) + K * parseInt(clientCountEl.value));
+    
+    let answer = up/down;
+    
+    resultEl.value = answer > 5 ? parseFloat('5').toFixed(2) : answer.toFixed(2);
+    resultEl.style.color = answer > goodRating ? main_clr : red;
+})
+
+// ====== ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ ======
+//функция для подсчета суммы всех полей input
+const checkSumWrong = function(list) {
+    let sum = 0;
+    for(let item of list) {
+        sum += Number(item.value);
+    }
+    return sum;
+}
 // функция перемножения количества оценок и баллов за оценку
 const calcReviews = (inputList) => {
     let sum = 0;
@@ -82,17 +101,6 @@ const calcReviews = (inputList) => {
     })
     return sum;
 }
-// вычисление рейтинга
-document.querySelector('#btn_result').addEventListener('click', () => {
-    let service = calcReviews(serviceReviewsEl) * parseInt(avgInputEl.value);
-    let client = K * calcReviews(clientReviewsEl) * parseInt(avgInputEl.value);
-
-    let up =  service + client;
-    let down = parseInt(avgInputEl.value) * (parseInt(serviceCountEl.value) + K * parseInt(clientCountEl.value));
-    
-    let answer = up/down;
-    resultEl.value = answer > 5 ? parseFloat('5').toFixed(2) : answer.toFixed(2);
-})
 
 
 
